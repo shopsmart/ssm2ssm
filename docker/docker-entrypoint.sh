c5bf04b3b1cc46a544fcc2da6f6ba1c05d1bebd8
Mike Carey <mike-carey@users.noreply.github.com>
2022-03-04 17:39:25 -0600

    Fixes entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-if [ -n "$1" ]; then
-  hash "$1" 2>/dev/null || {
-    echo "[DEBUG] Passing all arguments to ssm2ssm" >&2
-    ssm2ssm "${@:-}"
-    exit $?
-  }
+if [ -n "$1" ] && ! hash "$1" 2>/dev/null; then
+  echo "[DEBUG] Passing all arguments to ssm2ssm" >&2
+  ssm2ssm "${@:-}"
+  exit $?
 fi
 
 echo "[DEBUG] Running command as is"
